152b0bdf063c561b79fe9ec9b7d3f1f33614f01e
Paulo Welton <103148502+paulowelton@users.noreply.github.com>
2023-06-13 15:49:37 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+ // Adiciona o evento de clique ao botão "Adicionar"
+ document.getElementById("adicionarBtn").addEventListener("click", adicionarTarefa);
+
+ function adicionarTarefa() {
+   var input = document.getElementById("tarefaInput");
+   var tarefa = input.value;
+
+   if (tarefa !== "") {
+     var lista = document.getElementById("listaTarefas");
+     var item = document.createElement("li");
+     item.appendChild(document.createTextNode(tarefa));
+     lista.appendChild(item);
+
+     input.value = "";
+   }
+ }
